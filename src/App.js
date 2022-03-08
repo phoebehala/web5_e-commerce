ebafc6805acb807d7723e5d4ad97eef4ee1283b0
Phoebe <phyuhala@gmail.com>
2022-03-07 22:35:50 -0800

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,45 @@ import './app.css'
 
 // pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import ProductDetails from './pages/ProductDetails';
+import ProductDetailsDummy from './pages/ProductDetailsDummy';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
+
+// react route
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
 
 function App() {
+  const user = false
   return (
-  
-    <div className='app'>
-       <Home />
-    </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} >
+          <Route path=":category" element={<ProductList />} />   {/*  /products/:category */}
+        </Route>
+
+        <Route path="/product/:id" element={<ProductDetails />} />
+        
+        <Route path="/cart" element={<Cart />} />
+    
+
+        <Route path="/login" element={user? <Home /> :<Login />} />
+        <Route path="/register" element={user? <Home /> :<Register />} />
+      </Routes>
+
+
+    </Router>
+
+
  
   );
 }
