3a38c2c4e08468e98a1a14a9b67d3057b082d2f8
Phoebe <phyuhala@gmail.com>
2022-03-10 15:24:56 -0800

    added fakestore api data as dummy data

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,47 @@
+export const myProducts = [
+    {
+      "id": 1,
+      "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      "price": 109.95,
+      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      "category": "men's clothing",
+      
+      "rating": {
+          "rate": 3.9,
+          "count": 120
+      },
+
+      "src": [
+        "https://i.pinimg.com/474x/02/72/bb/0272bbd458acaac3c6761222effc6463.jpg",
+        "https://i.pinimg.com/474x/57/f3/51/57f35157ca63f1529d80af1685adb626.jpg",
+        "https://i.pinimg.com/474x/78/12/37/7812376350fc183addb64852e445092b.jpg",
+        "https://i.pinimg.com/474x/6c/1b/cd/6c1bcdf3fa796dc198506b008f25aae7.jpg"
+      ],
+
+      "colors":["red","black","crimson","teal"],
+
+      "info":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi magni veritatis reiciendis, harum perferendis? Ipsum expedita velit reiciendis corporis ex! Neque rem nulla ad, perspiciatis expedita vero explicabo pariatur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi magni veritatis reiciendis, harum perferendis? Ipsum expedita velit reiciendis corporis ex! Neque rem nulla ad, perspiciatis expedita vero explicabo pariatur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi magni veritatis reiciendis, harum perferendis? Ipsum expedita velit reiciendis corporis ex! Neque rem nulla ad, perspiciatis expedita vero explicabo pariatur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi magni veritatis reiciendis, harum perferendis? Ipsum expedita velit reiciendis corporis ex! Neque rem nulla ad, perspiciatis expedita vero explicabo pariatur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi magni veritatis reiciendis, harum perferendis? Ipsum expedita velit reiciendis corporis ex! Neque rem nulla ad, perspiciatis expedita vero explicabo pariatur?"
+    }
+]
+
 export const categories = [
     {
       id: 1,
       img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
-      cat:"man"
+      title: "jewelery",
+      cat:"jewelery"
     },
     {
       id: 2,
       img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
-      cat:"coat"
+      title: "women's clothing",
+      cat:"women's clothing"
     },
     {
       id: 3,
       img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
-      cat:"jeans"
+      title: "men's clothing",
+      cat:"men's clothing"
     },
   ];
 
