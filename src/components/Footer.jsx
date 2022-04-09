e3af144951bfb4fe6d3b09a10f63b65eafc5b90c
Phoebe <phyuhala@gmail.com>
2022-04-08 17:10:49 -0700

    updated Useful Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -152,13 +152,13 @@ const Footer = () => {
                 <Title>Useful Links</Title>
                 <List>
                     <ListItem>Home</ListItem>
-                    <ListItem>Cart</ListItem>
-                    <ListItem>Man Fashion</ListItem>
-                    <ListItem>Woman Fashion</ListItem>
-                    <ListItem>Accessories</ListItem>
+                    <ListItem>About Us</ListItem>
+                    <ListItem>Men's Clothing</ListItem>
+                    <ListItem>Women's Clothing</ListItem>
+                    <ListItem>Jewelery</ListItem>
                     <ListItem>My Account</ListItem>
                     <ListItem>Order Tracking</ListItem>
-                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Cart</ListItem>
                     <ListItem>Wishlist</ListItem>
                     <ListItem>Terms</ListItem>
                 </List>
@@ -180,7 +180,7 @@ const Footer = () => {
         </Wrapper>
 
             <Bottom>
-                <div style={{color:"gray"}}>©2021 Phoebe Yu</div>
+                <div style={{color:"gray"}}>©2022 Phoebe Yu</div>
 
             </Bottom>
         </Container>
